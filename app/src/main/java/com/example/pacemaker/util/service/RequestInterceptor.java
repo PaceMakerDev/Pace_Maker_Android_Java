00a5166cccb8f1091bf174877111364464b1be9b
Mountain96 <jtlsan@naver.com>
2021-04-21 21:22:42 +0900

    Add Utility classes for Okhttp service

---
+++ app/src/main/java/com/example/pacemaker/util/service/RequestInterceptor.java
@@ -0,0 +1,25 @@
+package com.example.pacemaker.util.service;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RequestInterceptor implements Interceptor {
+    private String authToken;
+
+    public RequestInterceptor(String authToken) {
+        this.authToken = authToken;
+    }
+    @NotNull
+    @Override
+    public Response intercept(@NotNull Chain chain) throws IOException {
+        Request original = chain.request();
+        Request.Builder builder = original.newBuilder().header("Authorization", authToken);
+        Request request = builder.build();
+        return chain.proceed(request);
+    }
+}
---
+++ app/src/main/java/com/example/pacemaker/util/service/ServiceGenerator.java
@@ -0,0 +1,36 @@
+package com.example.pacemaker.util.service;
+
+import android.text.TextUtils;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class ServiceGenerator {
+    //private static final String BASE_URL = "http://13.124.194.199:8080/";
+    private static final String BASE_URL = "http://10.0.2.2:8000/";
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    private static Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+    private static Retrofit retrofit = builder.build();
+
+    public static <S> S createService(Class<S> serviceClass) {
+        return createService(serviceClass, null);
+    }
+
+    public static <S> S createService(Class<S> serviceClass, String authToken) {
+        if (!TextUtils.isEmpty(authToken)) {
+            RequestInterceptor interceptor = new RequestInterceptor("Bearer " + authToken);
+
+            if (!httpClient.interceptors().contains(interceptor)) {
+                httpClient.addInterceptor(interceptor);
+
+                builder.client(httpClient.build());
+                retrofit = builder.build();
+            }
+        }
+
+        return retrofit.create(serviceClass);
+    }
+}
